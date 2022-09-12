5f775bbe3000f5528b5dd01d6d6b9db7014026f9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-12 20:18:07 +0300

    Added add_item method

--- classes/label.rb
+++ classes/label.rb
@@ -1,7 +1,13 @@
 class Label
-    def initialize(title ,color)
-        @title=title
-        @color = color
-
-    end
+    attr_accessor :title, :color, :items
+  def initialize(title, color)
+    @id = Random.rand(1...1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    
+  end
 end
