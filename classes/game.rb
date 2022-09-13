3a7edc43fcfefa29c1d50c1c82310fb084cd7b1f
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-13 15:32:09 +0100

    Add game_spec and attr_accessor to game.rb

--- classes/game.rb
+++ classes/game.rb
@@ -2,6 +2,8 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
